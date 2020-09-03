ed49ee69c742c03898cf0811a0068b401df913cb
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-09-03 22:10:39 +0200

    clean electron

--- mainElectron.js
+++ mainElectron.js
@@ -1,49 +1,12 @@
 /**
  * Main file to be launched by Electron
- * The command line "electron ." will launch this file
+ * The command line "electron <nameofthisfile>" will launch this file
  */
 
 const { app, BrowserWindow } = require('electron')
-const fs = require('fs');
 
 
 let win = null;
-let filename = undefined;
-
-function newDocument() {
-    filename = undefined;
-    win.title = "Zikpad - (new document)";
-    win.webContents.send("new");
-}
-
-function openDocument() {
-    let files = dialog.showOpenDialogSync({ title: "Open file", filters: [{ name: "Lilypond file", extensions: "ly" }] });
-    openFile(files[0]);
-
-}
-
-function openFile(filename) {
-    win.title = "Zikpad - " + filename;
-    fs.readFile(filename, 'utf8', (err, data) => {
-        win.webContents.send("open", data)
-    });
-}
-
-function saveDocument() {
-    if (filename == undefined)
-        saveAsDocument();
-    else
-        win.webContents.send("save");
-}
-
-function saveAsDocument() {
-    let newFilename = dialog.showSaveDialogSync({ title: "Save file", filters: [{ name: "Lilypond file", extensions: "ly" }] });
-    if (newFilename == undefined) return;
-
-    filename = newFilename;
-    win.title = "Zikpad - " + filename;
-    saveDocument();
-}
 
 app.on('ready', () => {
     win = new BrowserWindow({
@@ -51,9 +14,8 @@ app.on('ready', () => {
         width: 1200,
         height: 850,
         fullscreen: true,
-        //   icon: 'assets/zds.png',
+        icon: 'img/logo.png',
         title: 'Tableaunoir',
-        webPreferences: { nodeIntegration: true },
         movable: false
     });
 
