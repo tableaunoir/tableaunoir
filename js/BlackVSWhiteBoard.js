05e499fe83f286c8d1312139ded35e8de0f7c3cb
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-10-18 17:23:50 +0200

    sim city graph

--- js/BlackVSWhiteBoard.js
+++ js/BlackVSWhiteBoard.js
@@ -26,7 +26,7 @@ class BlackVSWhiteBoard {
         var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
         var data = imageData.data;
 
-        for (var i = 0; i < data.length; i += 4) {
+        for (let i = 0; i < data.length; i += 4) {
             // red
             data[i] = 255 - data[i];
             // green
