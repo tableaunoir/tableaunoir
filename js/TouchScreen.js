035c5a7cafc88a30c088a6bb469ec0319994456a
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-10-25 15:15:31 +0100

    touch

---
+++ js/TouchScreen.js
@@ -0,0 +1,38 @@
+class TouchScreen {
+
+    static addTouchEvents(element) {
+        element.ontouchstart = TouchScreen._touchHandler;
+        element.ontouchmove = TouchScreen._touchHandler;
+        element.ontouchend = TouchScreen._touchHandler;
+        element.ontouchcancel = TouchScreen._touchHandler;
+    }
+    /**
+     * 
+     * @param {*} event 
+     * @description converts a touch event into a mouse event
+     */
+    static _touchHandler(event) {
+        var touches = event.changedTouches,
+            first = touches[0],
+            type = "";
+        switch (event.type) {
+            case "touchstart": type = "mousedown"; break;
+            case "touchmove": type = "mousemove"; break;
+            case "touchend": type = "mouseup"; break;
+            default: return;
+        }
+
+        // initMouseEvent(type, canBubble, cancelable, view, clickCount, 
+        //                screenX, screenY, clientX, clientY, ctrlKey, 
+        //                altKey, shiftKey, metaKey, button, relatedTarget);
+
+        var simulatedEvent = document.createEvent("MouseEvent");
+        simulatedEvent.initMouseEvent(type, true, true, window, 1,
+            first.screenX, first.screenY,
+            first.clientX, first.clientY, false,
+            false, false, false, 0/*left*/, null);
+
+        first.target.dispatchEvent(simulatedEvent);
+        event.preventDefault();
+    }
+}
