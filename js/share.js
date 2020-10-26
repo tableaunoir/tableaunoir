aa5717c2f627aba1c166149a9f302e610acbacf0
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-10-26 20:00:57 +0100

    server

--- js/share.js
+++ js/share.js
@@ -3,25 +3,31 @@ class Share {
 	static id = undefined;
 
 	static init() {
-		Share.ws = new WebSocket('ws://tableaunoir.irisa.fr:443');
-		Share.ws.binaryType = "arraybuffer";
-
-		Share.ws.onopen = () => { Share.tryJoin(); };
-		Share.ws.onmessage = (msg) => {
-			console.log("I received the message: ");
-			console.log(msg)
-			Share._treatReceivedMessage(JSON.parse(msg.data));
-		};
-
-		document.getElementById("shareButton").onclick = () => {
-			if (!Share.isShared())
-				Share.share();
-		};
-
-		setTimeout(() => {
-			if (!Share.isShared())
-				Share.share()
-		}, 1000); //just for test. Should be removed at the end (the button share does it)
+		try {
+			Share.ws = new WebSocket('ws://tableaunoir.irisa.fr:443');
+			Share.ws.binaryType = "arraybuffer";
+	
+			Share.ws.onopen = () => { Share.tryJoin(); };
+			Share.ws.onmessage = (msg) => {
+				console.log("I received the message: ");
+				console.log(msg)
+				Share._treatReceivedMessage(JSON.parse(msg.data));
+			};
+	
+			document.getElementById("shareButton").onclick = () => {
+				if (!Share.isShared())
+					Share.share();
+			};
+	
+			setTimeout(() => {
+				if (!Share.isShared())
+					Share.share()
+			}, 1000); //just for test. Should be removed at the end (the button share does it)
+		}
+		catch(e) {
+			
+		}
+		
 	}
 
 
