b27ceef18a9180247e6f974b992bed3ff85781d6
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-10-26 17:56:10 +0100

    share

--- js/share.js
+++ js/share.js
@@ -1,20 +1,26 @@
 class Share {
 	static ws;
+	static id = undefined;
 
 	static init() {
 		Share.ws = new WebSocket('ws://tableaunoir.irisa.fr:8080');
 		
 		Share.ws.onmessage = (msg) => {
-			console.log("I received ");
-			console.log(msg)
+			console.log("I received the data: ");
+			console.log(msg.data)
 		};
-		
+
 		setTimeout(() => Share.share(), 1000);
 	}
 
 
 	static share() {
 		this.ws.send("share");
+	}
+
+
+	static join(id) {
+
 	}
 }
 
