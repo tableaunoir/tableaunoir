4d9841361db58a4f49f7df6af8da0333674b8efe
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-11-02 20:30:06 +0100

    try to make it responsive

--- js/Toolbar.js
+++ js/Toolbar.js
@@ -2,6 +2,11 @@ class Toolbar {
     static init() {
         if (Layout.isTactileDevice()) {
             buttonCloseControls.hidden = true; //on phone or tablet, we can not remove the toolbar, therefore the button is hidden
+
+            for(let o of document.querySelectorAll("#controls > span")) {
+                o.hidden = true;
+            }
+                
         }
     }
 
