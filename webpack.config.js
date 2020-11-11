7026c834d6d9efc4368a70c2fd725dba646302f9
Morandat <florealm@gmail.com>
2020-11-11 22:31:12 +0100

    Added webpack support

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+const path = require('path')
+
+module.exports = {
+  entry: {
+    tableaunoir: [
+      "./src/main.ts",
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "tableaunoir.js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: ["ts-loader"], exclude: /node_modules/ },
+    ],
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+      DEBUG: false
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: 'index.html', to: 'index.html' }, // Use html webpack ?
+        { from: 'style.css', to: 'style.css' }, // Use css loader ?
+        { from: 'img', to: 'img' }, // Use image loader ?
+        { from: 'lib', to: 'lib' },
+      ],
+    })
+  ]
+};
