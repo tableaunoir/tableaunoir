fa1ca6090cfa42edfec9b39b3b70bd578a7be139
Morandat <florealm@gmail.com>
2020-11-12 13:33:25 +0100

    Import {} is not import

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
