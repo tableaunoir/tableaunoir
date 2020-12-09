8c0ba36bb4e9fd776e9eca1c20e824477bd12eca
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-12-09 09:53:18 +0100

    undo dot ok

--- src/ActionFreeDraw.ts
+++ src/ActionFreeDraw.ts
@@ -3,18 +3,23 @@ import { getCanvas } from './main';
 import { Action } from './Action';
 
 export class ActionFreeDraw implements Action {
-
+    public alreadyDrawnSth = false;
     private points: { x: number; y: number; pressure: number; color: string; }[] = [];
 
     addPoint(pt: { x: number; y: number; pressure: number; color: string; }) {
         this.points.push(pt);
+        if (Math.abs(pt.x - this.points[0].x) > 1 || Math.abs(pt.y - this.points[0].y) > 1)
+            this.alreadyDrawnSth = true;
     }
 
     async redo(): Promise<void> {
+        if (!this.alreadyDrawnSth)
+            Drawing.drawDot(this.points[0].x, this.points[0].y, this.points[0].color);
+
         for (let i = 1; i < this.points.length; i++) {
-            Drawing.drawLine(getCanvas().getContext("2d"), this.points[i-1].x,  this.points[i-1].y,  this.points[i].x, this.points[i].y, this.points[i].pressure, this.points[i].color);
+            Drawing.drawLine(getCanvas().getContext("2d"), this.points[i - 1].x, this.points[i - 1].y, this.points[i].x, this.points[i].y, this.points[i].pressure, this.points[i].color);
         }
-        
+
     }
 
 }
