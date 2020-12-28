2868400b90bc6ebe91433c163defd9a7b1c8e936
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-12-28 18:54:45 +0100

    snapshot of the pdf #90

--- src/PDFViewer.ts
+++ src/PDFViewer.ts
@@ -3,17 +3,17 @@ import { PDFDocument } from './PDFDocument';
 export class PDFViewer {
 
     constructor(pdfdoc: PDFDocument) {
-        
-        this.inputPDFNumPage.onchange = async (ev) => {
+        const f = async () => {
             const i = this.numPage;
             const canvas = await pdfdoc.getCanvasPage(i);
             canvas.style.height = "300px";
             canvas.style.position = "relative"; //for the canvas to "take place"
             document.getElementById("pdfSnapshot").innerHTML = "";
             document.getElementById("pdfSnapshot").appendChild(canvas);
-        }
-
+        };
+        this.inputPDFNumPage.onchange = f;
         this.inputPDFNumPage.value = "1";
+        f();
     }
 
     get numPage(): number {
