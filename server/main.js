18a7097b0cddc123fba77bee87733d3e09c9bec4
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-01-24 14:28:59 +0100

    solving bug in tableaus' ids

--- server/main.js
+++ server/main.js
@@ -187,7 +187,13 @@ class TableauNoir {
    * @returns generate an ID for a new tableaunoir
    */
   static generateTableauID() {
-    return uuid.create();//"t" + generateID();
+    const s = uuid.create();
+    return s.replace(/+/g, ""); 
+    
+    /*delete all the + signs because it causes a bug in the extraction of the ID in the URL
+    For an unknown reason, (new URL(document.location.href)).searchParams.get('id') transforms all + into space.
+    */
+
   }
 
 
