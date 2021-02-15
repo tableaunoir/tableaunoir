b01bc91d01e905af3254321939973a71abbd9908
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-02-15 21:23:51 +0100

    msg cleaning cache if needed

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ErrorMessage } from './ErrorMessage';
 import { GUIActions } from './GUIActions';
 import { Sound } from './Sound';
 import { TestPerformance } from './TestPerformance';
@@ -20,7 +21,7 @@ import { Menu } from './Menu';
 import { TouchScreen } from './TouchScreen';
 import { Drawing } from './Drawing';
 import { KeyBoardShortCuts } from './KeyBoardShortCuts';
-import { S , Script} from './Script';
+import { S, Script } from './Script';
 import { Export } from './Export';
 
 TestPerformance.init();
@@ -50,75 +51,81 @@ function installBodyNoScroll() {
 
 
 function load() {
-	installBodyNoScroll();
+	try {
 
-	UserManager.init();
+		installBodyNoScroll();
 
-
-	Background.init();
-	Layout.init();
-
-	Translation.init();
-	ChalkCursor.init();
-	LoadSave.init();
-	Export.init();
-	BoardManager.init();
-	Menu.init();
-	Share.init();
-	Toolbar.init();
-	Discussion.init();
-	CSSStyleModifier.init();
-	Drawing.init();
-	BoardNavigation.init();
-	BlackVSWhiteBoard.init();
-	GUIActions.init();
-	MagnetManager.init();
-	MyMagnets.loadMagnets();
-	Sound.init();
-	Script.init();
-
-	BoardManager.load();
+		UserManager.init();
 
 
-	document.getElementById("buttonMenu").onclick = Menu.toggle;
-	document.getElementById("buttonColors").onclick = () => GUIActions.changeColor();
+		Background.init();
+		Layout.init();
 
-	document.getElementById("buttonChalk").onclick = GUIActions.switchChalkEraser;
-	document.getElementById("buttonEraser").onclick = GUIActions.switchChalkEraser;
+		Translation.init();
+		ChalkCursor.init();
+		LoadSave.init();
+		Export.init();
+		BoardManager.init();
+		Menu.init();
+		Share.init();
+		Toolbar.init();
+		Discussion.init();
+		CSSStyleModifier.init();
+		Drawing.init();
+		BoardNavigation.init();
+		BlackVSWhiteBoard.init();
+		GUIActions.init();
+		MagnetManager.init();
+		MyMagnets.loadMagnets();
+		Sound.init();
+		Script.init();
 
-	document.getElementById("buttonTools").onclick = () => {
-		GUIActions.toolmenu.show({ x: UserManager.me.x, y: UserManager.me.y });
+		BoardManager.load();
+
+
+		document.getElementById("buttonMenu").onclick = Menu.toggle;
+		document.getElementById("buttonColors").onclick = () => GUIActions.changeColor();
+
+		document.getElementById("buttonChalk").onclick = GUIActions.switchChalkEraser;
+		document.getElementById("buttonEraser").onclick = GUIActions.switchChalkEraser;
+
+		document.getElementById("buttonTools").onclick = () => {
+			GUIActions.toolmenu.show({ x: UserManager.me.x, y: UserManager.me.y });
+		}
+
+		document.getElementById("buttonText").onclick = () => MagnetManager.addMagnetText(UserManager.me.x, UserManager.me.y);
+		document.getElementById("buttonDivide").onclick = () => Share.execute("divideScreen", [UserManager.me.userID, Layout.getXMiddle()]);
+
+		document.getElementById("buttonLeft").onclick = BoardNavigation.leftPreviousPage;
+
+		document.getElementById("buttonRight").onclick = BoardNavigation.rightNextPage;
+		document.getElementById("buttonMap").onclick = () => {
+			if (Layout.isMinimap)
+				Layout.normal();
+			else
+				Layout.minimap();
+		};
+		document.getElementById("buttonCancel").onclick = () => Share.execute("cancel", [UserManager.me.userID]);
+		document.getElementById("buttonRedo").onclick = () => Share.execute("redo", [UserManager.me.userID]);
+
+		document.getElementById("buttonAskQuestion").onclick = Discussion.askQuestion;
+
+		const buttons = document.getElementById("controls").children;
+
+		for (let i = 0; i < buttons.length; i++)
+			if (buttons[i] instanceof HTMLButtonElement)
+				(<HTMLButtonElement>buttons[i]).onfocus = (<HTMLElement>document.activeElement).blur; //to be improved
+
+		document.onkeydown = KeyBoardShortCuts.onKeyDown;
+
+
+		document.getElementById("canvasBackground").onpointermove = () => { console.log("mousemove on the background should not occur") };
+
+		installMouseEventsCanvas();
 	}
-
-	document.getElementById("buttonText").onclick = () => MagnetManager.addMagnetText(UserManager.me.x, UserManager.me.y);
-	document.getElementById("buttonDivide").onclick = () => Share.execute("divideScreen", [UserManager.me.userID, Layout.getXMiddle()]);
-
-	document.getElementById("buttonLeft").onclick = BoardNavigation.leftPreviousPage;
-
-	document.getElementById("buttonRight").onclick = BoardNavigation.rightNextPage;
-	document.getElementById("buttonMap").onclick = () => {
-		if (Layout.isMinimap)
-			Layout.normal();
-		else
-			Layout.minimap();
-	};
-	document.getElementById("buttonCancel").onclick = () => Share.execute("cancel", [UserManager.me.userID]);
-	document.getElementById("buttonRedo").onclick = () => Share.execute("redo", [UserManager.me.userID]);
-
-	document.getElementById("buttonAskQuestion").onclick = Discussion.askQuestion;
-
-	const buttons = document.getElementById("controls").children;
-
-	for (let i = 0; i < buttons.length; i++)
-		if (buttons[i] instanceof HTMLButtonElement)
-			(<HTMLButtonElement>buttons[i]).onfocus = (<HTMLElement>document.activeElement).blur; //to be improved
-
-	document.onkeydown = KeyBoardShortCuts.onKeyDown;
-
-
-	document.getElementById("canvasBackground").onpointermove = () => { console.log("mousemove on the background should not occur") };
-
-	installMouseEventsCanvas();
+	catch (e) {
+		ErrorMessage.show("Problem in loading Tableaunoir... please try to clean the cache of your browser.");
+	}
 
 }
 
