6865c4408c03d950e22f1902f415d92b766a797b
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-04-22 19:47:00 +0200

    export png more precise

--- src/Export.ts
+++ src/Export.ts
@@ -21,16 +21,36 @@ export class Export {
         CircularMenu.hideAndRemove();
         const nodeContent = document.getElementById("content");
         Layout.setForExportPng();
-        html2canvas(nodeContent).then(canvas => {
+        html2canvas(nodeContent).then(bigCanvas => {
+            console.log(BoardManager.width);
+            const canvas = Export.extractFromCanvas(bigCanvas, 0, BoardManager.width);
             LoadSave.downloadDataURL((<HTMLInputElement>document.getElementById("exportPngName")).value + ".png", canvas.toDataURL());
         });
         Layout.restoreForUse();
     }
 
 
-    /**
- * @description export the board as a .png image file
+/**
+ * 
+ * @param bigCanvas 
+ * @param x 
+ * @param w 
+ * @returns a canvas contains only the portion between left x and right x + w
  */
+    private static extractFromCanvas(bigCanvas: HTMLCanvasElement, x: number, w: number): HTMLCanvasElement {
+        const canvasPage = document.createElement("canvas");
+        const h = Layout.getWindowHeight();
+        console.log(bigCanvas.height)
+        canvasPage.height = h;
+        canvasPage.width = w;
+        const scaleCorrection = bigCanvas.height / h; //because the html2canvas canvas is of height 1090 instead of 1000. Why?
+        canvasPage.getContext("2d").drawImage(bigCanvas, x * scaleCorrection, 0, w * scaleCorrection, h * scaleCorrection, 0, 0, w, h);
+        return canvasPage;
+    }
+
+    /**
+      * @description export the board as a .png image file
+     */
     static exportPdf(): void {
         CircularMenu.hideAndRemove();
         const nodeContent = document.getElementById("content");
@@ -41,17 +61,13 @@ export class Export {
             for (let x = 0; x < BoardManager.width; x += Layout.getWindowWidth()) {
                 if (!firstpage)
                     doc.addPage();
-                const canvasPage = document.createElement("canvas");
-                const h = Layout.getWindowHeight();
-                const w = Layout.getWindowWidth();
-                console.log(canvas.height)
-                canvasPage.height = h;
-                canvasPage.width = w;
-                const scaleCorrection = canvas.height / h; //because the html2canvas canvas is of height 1090 instead of 1000. Why?
-                canvasPage.getContext("2d").drawImage(canvas, x*scaleCorrection, 0, w*scaleCorrection, h*scaleCorrection, 0, 0, w, h);
-                
+                const canvasPage = Export.extractFromCanvas(canvas, x, Layout.getWindowWidth());
+
                 const pw = doc.internal.pageSize.getWidth();
                 const ph = doc.internal.pageSize.getHeight();
+                const h = canvasPage.height;
+                const w = canvasPage.width;
+
                 if (w / h > pw / ph)
                     doc.addImage(canvasPage, 0, 0, pw, pw * h / w);
                 else
