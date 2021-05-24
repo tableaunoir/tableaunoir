c74732e9e83baeb79b5a949aff547385eee55f5a
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-05-24 16:15:45 +0200

    #161 fixed

--- src/ConstraintDrawing.ts
+++ src/ConstraintDrawing.ts
@@ -19,51 +19,85 @@ export class ConstraintDrawing {
         const magnet2id = svgLine.dataset.magnet2id;
 
         if (magnet1id && magnet2id) {
+            if (magnet1id == magnet2id)
+                ConstraintDrawing.constraints.push(() => {
+                    const magnet1 = document.getElementById(magnet1id);
 
-            ConstraintDrawing.constraints.push(() => {
-                const magnet1 = document.getElementById(magnet1id);
-                const magnet2 = document.getElementById(magnet2id);
-
-                if (magnet1 == undefined || magnet2 == undefined) {
-                    svgLine.remove();
-                    return false;
-                }
-
-                const pm1 = MagnetManager.getMagnetCenter(magnet1);
-                const pm2 = MagnetManager.getMagnetCenter(magnet2);
-
-                const getSize = (magnet) => Math.min(magnet.clientHeight, magnet.clientWidth);
-                const size1 = getSize(magnet1);
-                const size2 = getSize(magnet2);
-
-                const dtotal = parseFloat(svgLine.dataset.d);
-                const dm = (magnet1 == magnet2) ? 1 : Geometry.distance(pm1, pm2);
-                const anglem = (magnet1 == magnet2) ? 0 : Geometry.angle(pm1, pm2);
-
-                const getNewPosition = (d, angle) => {
-                    if (d < size1) {
-                        return Geometry.polar(pm1, d, anglem + angle);
-                    }
-                    else if (d > dtotal - size2)
-                        return Geometry.polar(pm2, -(dtotal - d), anglem + angle);
-                    else {
-                        const dratio = (d - size1) / (dtotal - size1 - size2);
-                        return Geometry.polar(pm1, size1 + dratio * (dm - size1 - size2), anglem + angle);
+                    if (magnet1 == undefined) {
+                        svgLine.remove();
+                        return false;
                     }
 
+                    const pm1 = MagnetManager.getMagnetCenter(magnet1);
 
 
-                };
 
-                const p1 = getNewPosition(parseFloat(svgLine.dataset.d1), parseFloat(svgLine.dataset.angle1));
-                const p2 = getNewPosition(parseFloat(svgLine.dataset.d2), parseFloat(svgLine.dataset.angle2));
+                    const getNewPosition = (x: number, y: number) => {
+                        return { x: pm1.x + x, y: pm1.y + y };
+                    };
 
-                svgLine.setAttributeNS(null, 'x1', "" + p1.x);
-                svgLine.setAttributeNS(null, 'y1', "" + p1.y);
-                svgLine.setAttributeNS(null, 'x2', "" + p2.x);
-                svgLine.setAttributeNS(null, 'y2', "" + p2.y);
-                return true;
-            });
+                    const p1 = getNewPosition(parseFloat(svgLine.dataset.refx1), parseFloat(svgLine.dataset.refy1));
+                    const p2 = getNewPosition(parseFloat(svgLine.dataset.refx2), parseFloat(svgLine.dataset.refy2));
+
+                    svgLine.setAttributeNS(null, 'x1', "" + p1.x);
+                    svgLine.setAttributeNS(null, 'y1', "" + p1.y);
+                    svgLine.setAttributeNS(null, 'x2', "" + p2.x);
+                    svgLine.setAttributeNS(null, 'y2', "" + p2.y);
+                    return true;
+                });
+            else //two different magnets
+
+                ConstraintDrawing.constraints.push(() => {
+                    const magnet1 = document.getElementById(magnet1id);
+                    const magnet2 = document.getElementById(magnet2id);
+
+                    if (magnet1 == undefined || magnet2 == undefined) {
+                        svgLine.remove();
+                        return false;
+                    }
+
+                    const pm1 = MagnetManager.getMagnetCenter(magnet1);
+                    const pm2 = MagnetManager.getMagnetCenter(magnet2);
+
+                    const getSize = (magnet) => Math.min(magnet.clientHeight, magnet.clientWidth);
+                    const size1 = getSize(magnet1);
+                    const size2 = getSize(magnet2);
+
+                    const refd = parseFloat(svgLine.dataset.refd);
+                    const d = Geometry.distance(pm1, pm2);
+
+                    const getNewPosition = (refx: number, refy: number) => {
+                        const vectorDirection = Geometry.normalize({ x: pm2.x - pm1.x, y: pm2.y - pm1.y });
+                        const vectorOrtho = { x: -vectorDirection.y, y: vectorDirection.x };
+
+                        const pos = (x: number, y: number) => {
+                            return {
+                                x: pm1.x + vectorDirection.x * x + vectorOrtho.x * y,
+                                y: pm1.y + vectorDirection.y * x + vectorOrtho.y * y
+                            }
+                        };
+
+                         if (refx < size1) {
+                             return pos(refx, refy);
+                         }
+                         else if (refx > refd - size2)
+                             return pos(d - (refd - refx), refy);
+                         else {
+                             const dratio = (refx - size1) / (refd - size1 - size2);
+                             return pos(size1 + dratio * (d - size1 - size2), refy);
+                         }
+                        //return pos(refx * d / refd, refy);
+                    };
+
+                    const p1 = getNewPosition(parseFloat(svgLine.dataset.refx1), parseFloat(svgLine.dataset.refy1));
+                    const p2 = getNewPosition(parseFloat(svgLine.dataset.refx2), parseFloat(svgLine.dataset.refy2));
+
+                    svgLine.setAttributeNS(null, 'x1', "" + p1.x);
+                    svgLine.setAttributeNS(null, 'y1', "" + p1.y);
+                    svgLine.setAttributeNS(null, 'x2', "" + p2.x);
+                    svgLine.setAttributeNS(null, 'y2', "" + p2.y);
+                    return true;
+                });
 
         }
     }
@@ -80,38 +114,42 @@ export class ConstraintDrawing {
      * @description adds constraints for the svgLines to be between magnet1 and magnet2 
      */
     static freeDraw(svgLines: SVGLineElement[], magnet1id: string, magnet2id: string): void {
-        const magnet1 = document.getElementById(magnet1id);
-        const magnet2 = document.getElementById(magnet2id);
 
+        const magnet1 = document.getElementById(magnet1id);
         const pm1 = MagnetManager.getMagnetCenter(magnet1);
+
+        const magnet2 = document.getElementById(magnet2id);
         const pm2 = MagnetManager.getMagnetCenter(magnet2);
-        const dm = (magnet1 == magnet2) ? 1 : Geometry.distance(pm1, pm2);
-        const anglem = (magnet1 == magnet2) ? 0 : Geometry.angle(pm1, pm2);
+
+        const d = (magnet1 == magnet2) ? 1 : Geometry.distance(pm1, pm2);
+
+        let getInfoFromPoint: (p: { x: number, y: number }) => { x: number, y: number };
+        if (magnet1id == magnet2id)
+            getInfoFromPoint = (p: { x: number, y: number }) => ({ x: p.x - pm1.x, y: p.y - pm1.y });
+        else
+            getInfoFromPoint = (p: { x: number, y: number }) => Geometry.getNewCoordinates(p, pm1, pm2);
 
         for (const svgLine of svgLines) {
-            const p1 = { x: parseInt(svgLine.getAttributeNS(null, 'x1')), y: parseInt(svgLine.getAttributeNS(null, 'y1')) };
-            const p2 = { x: parseInt(svgLine.getAttributeNS(null, 'x2')), y: parseInt(svgLine.getAttributeNS(null, 'y2')) };
+            const p1 = { x: parseFloat(svgLine.getAttributeNS(null, 'x1')), y: parseFloat(svgLine.getAttributeNS(null, 'y1')) };
+            const p2 = { x: parseFloat(svgLine.getAttributeNS(null, 'x2')), y: parseFloat(svgLine.getAttributeNS(null, 'y2')) };
 
-            const d1 = Geometry.distance(pm1, p1);
-            const angle1 = Geometry.angle(pm1, p1) - anglem;
+            const info1 = getInfoFromPoint(p1);
+            const info2 = getInfoFromPoint(p2);
 
+            /**
+             * store the information of the constraint in the SVG object
+             */
             svgLine.dataset.magnet1id = magnet1id;
             svgLine.dataset.magnet2id = magnet2id;
-            svgLine.dataset.d1 = "" + d1;
-            svgLine.dataset.angle1 = "" + angle1;
-            svgLine.dataset.d = "" + dm;
-
-            const d2 = Geometry.distance(pm1, p2);
-            const angle2 = Geometry.angle(pm1, p2) - anglem;
-
-            svgLine.dataset.d2 = "" + d2;
-            svgLine.dataset.angle2 = "" + angle2;
+            svgLine.dataset.refd = "" + d;
+            svgLine.dataset.refx1 = "" + info1.x;
+            svgLine.dataset.refy1 = "" + info1.y;
+            svgLine.dataset.refx2 = "" + info2.x;
+            svgLine.dataset.refy2 = "" + info2.y;
 
             ConstraintDrawing.line(svgLine);
         }
-
     }
-
 
     /**
      * @description apply the constraint
--- src/Geometry.ts
+++ src/Geometry.ts
@@ -3,8 +3,21 @@ export class Geometry {
         return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
     }
 
-    static angle(p1: { x: number, y: number }, p2: { x: number, y: number }): number {
-        return Math.atan2(p2.y - p1.y, p2.x - p1.x);
+    static norm(vector: { x: number, y: number }): number {
+        return Math.sqrt(vector.x ** 2 + vector.y ** 2);
+    }
+
+    static normalize(vector: { x: number, y: number }): { x: number, y: number } {
+        const norm = Geometry.norm(vector);
+        return { x: vector.x / norm, y: vector.y / norm };
+    }
+
+    static angle(u: { x: number, y: number }, v: { x: number, y: number }): number {
+       /* u = Geometry.normalize(u);
+        v = Geometry.normalize(v);
+        const scalarProduct = u.x * v.x + u.y * v.y;
+        return Math.acos(scalarProduct);*/
+        return Math.atan2(u.y, u.x) - Math.atan2(v.y, v.x);
     }
 
     static polar(center: { x: number, y: number }, distance: number, angle: number): { x: number, y: number } {
@@ -16,6 +29,24 @@ export class Geometry {
     }
 
     static numberRound(x: number): number {
-        return Math.round(10*x)/10;
+        return Math.round(10 * x) / 10;
+    }
+
+    /**
+     * 
+     * @param point 
+     * @param origin 
+     * @param direction 
+     * @returns the coordinates "x" along the vector origin->direction, and "y" along the vector orthogonal to origin->direction 
+     */
+    static getNewCoordinates(point: { x: number, y: number }, origin: { x: number, y: number }, direction: { x: number, y: number }): { x: number, y: number } {
+        const directionVector = { x: direction.x - origin.x, y: direction.y - origin.y };
+        const distance = Geometry.distance(origin, point);
+        const OP = { x: point.x - origin.x, y: point.y - origin.y };
+        const angle = Geometry.angle(OP, directionVector);
+        return {
+            x: distance * Math.cos(angle),
+            y: distance * Math.sin(angle)
+        };
     }
 }
