50faf6855e00ae04dd542f0a7c1161c51ce24324
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-07-27 10:53:40 +0200

    pause/unpause action is undoable

---
+++ src/OperationPauseAction.ts
@@ -0,0 +1,18 @@
+import { AnimationToolBar } from './AnimationToolBar';
+import { BoardManager } from './boardManager';
+import { Operation } from "./Operation";
+
+export class OperationPauseAction extends Operation {
+
+    constructor(private t: number) { super() }
+
+    private do(): void {
+        const action = BoardManager.timeline.actions[this.t];
+        action.pause = !action.pause;
+        AnimationToolBar.update();
+    }
+
+    undo(): void { this.do() }
+    redo(): void { this.do() }
+
+}
